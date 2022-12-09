4320d731282c1d8e5508f4520783c00289aea57c
Michelle Ammerman <mammer@umich.edu>
2022-12-09 11:25:06 -0500

    track data analysis

---
+++ code/un_data_analysis_R.R
@@ -0,0 +1,18 @@
+# Get started ----
+# Yesterday we spent time  making plots using ggplot
+# what if we want to work on subsection
+# clean up messy data
+# create a new variable
+# we are going to use dplyr
+# need to load data ----
+gapminder_data <- read_csv("data/gapminder_data.csv")
+
+library(tidyverse)
+gapminder_data <- read_csv("data/gapminder_data.csv")
+library(tidyverse)
+gapminder_data <- read_csv("data/gapminder_data.csv")
+cd Desktop/un-report/data
+library(tidyverse)
+gapminder_data <- read_csv("data/gapminder_data.csv")
+library(tidyverse)
+gapminder_data <- read_csv("data/gapminder_data.csv")
